5d21a28982c3ee40313847002326c6fb13421a85
smjavia <smjavia@PC26.ICTILFS.COM>
2020-05-17 16:57:17 +0530

    Created angular Project in angular 8. also make a service method to get book list. Commit Date  17/05/2020

--- onlinebookstore/src/main/java/in/abhishek/onlinebookstore/Repository/BookRepository.java
+++ onlinebookstore/src/main/java/in/abhishek/onlinebookstore/Repository/BookRepository.java
@@ -1,9 +1,11 @@
 package in.abhishek.onlinebookstore.Repository;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.web.bind.annotation.CrossOrigin;
 
 import in.abhishek.onlinebookstore.Entity.Book;
 
+@CrossOrigin("*") //you can put * instead of this
 public interface BookRepository extends JpaRepository<Book, Integer> 
 {
 
